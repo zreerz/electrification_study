14e6e9ad0482178394e29262760c44be946b3b5d
Alyona Ivanova <Ivanova.alyona5@gmail.com>
2019-08-14 13:50:28 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,13 @@
 gridlabd --version
+### Check if a directory does not exist ###
+if [ ! -d "model_files/" ] 
+then
+    mkdir model_files
+fi
+if [ ! -d "output/" ] 
+then
+    mkdir output
+fi
 python3 main.py
 for filename in model_files/*.glm; do 
 	gridlabd "$filename"
