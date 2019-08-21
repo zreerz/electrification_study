67095afcbf42fac284c80038f8cac052be2cd76d
aivanova5 <ivanova.alyona5@gmail.com>
2019-08-21 10:14:32 -0700

    Updated the config file

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ config_files=[]
 with open('config/appliance_config.csv', newline='') as csvfile : 
 	fr = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in fr : 
-		if "Total Number of Houses" in row[0] : 
-			total_count = int(int(row[1])/3) #count per phase
+		if "Total Number of Houses per Phase" in row[0] : 
+			total_count = int(row[1]) #count per phase
 		elif 'Run Name' in row[0] : 
 			continue 
 		else :
-			elec_count = int(total_count*float(row[1]))
-			gas_count = int(total_count-elec_count)
-			if elec_count+gas_count != total_count : 
-				elec_count = elec_count + 1
+			elec_count = round(total_count*float(row[1]))
+			gas_count = total_count-elec_count
+			print(elec_count, gas_count)
 			config_file_name = 'elec_config_'+str(row[0]).replace(" ", "_")+'.glm'
 			config_files.append(config_file_name)
 			fw = open("elec_config/"+config_file_name, 'w')
-			fw.write('#define HOUSESPERPHASE=' + str(int(total_count)))
+			fw.write('#define HOUSESPERPHASE=' + str(total_count))
 			fw.write('\n#define GAS_COUNT=' + str(gas_count))
 			fw.write('\n#define ELEC_COUNT=' + str(elec_count))
 
@@ -43,7 +42,6 @@ for i,file_name in enumerate(config_files) :
 			fm.seek(0, 0)
 			line="#include \"elec_config/" + file_name + "\"" 
 			fm.write(line+"\n"+"#define RUN_NAME="+file_name[12:-4]+"\n")
-
 
 
 
--- post_process.py
+++ post_process.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+import operator
 
 paneldump_path='paneldump/'
 volt_val = 1000/110 # kW/V to A conversion fraction
@@ -12,35 +14,40 @@ f_list = []
 onlyfiles = [f for f in listdir(paneldump_path) if isfile(join(paneldump_path, f)) and f.startswith( "paneldump" )]
 def paneldumpparse(f_name) : 
 	curr_list = []
+	curr_list_for_print = []
 	house_curr = {}
-	house_dict = {} # {house_# : { time : power }}
+	house_dict = {} # {house_# : { timestamp : power }}
+	time_dict = {} # {timestamp : { house : power }}
 	with open(paneldump_path+f_name, newline='') as csvfile : 
 		fr = csv.reader(csvfile, delimiter=',', quotechar='|')
 		next(fr)
 		for row in fr :
 			curr_val = float(row[3])*volt_val
-			if not house_dict : # first line of dictionary
-				house_dict[row[1]] = {row[0]:curr_val}
+			if not time_dict : # first line of dictionary
+				time_dict[row[0]] = {row[1]:curr_val}
 			else : 
-				if row[1] in house_dict : # check if house is in list 
-					if row[0] in house_dict[row[1]].keys() : # check if timestamp is in the list
-						house_dict[row[1]][row[0]] = float(house_dict[row[1]][row[0]])+curr_val
-						# if '2018-01-15 02:00:00 PST' in row[0] and 'house_99_ELEC' in row[1]:
-							# print(row[0],row[1],float(house_dict[row[1]][row[0]]))
+				if row[0] in time_dict : # check if timestamp is in list 
+					if row[1] in time_dict[row[0]].keys() : # check if timestamp is in the list
+						time_dict[row[0]][row[1]] = float(time_dict[row[0]][row[1]])+curr_val
 					else : #add new house key
-						house_dict[row[1]][row[0]]=curr_val
+						time_dict[row[0]][row[1]]=curr_val
 				else : #timestamp not in list
-					house_dict[row[1]]={row[0]:curr_val}
-					# if '2018-01-15 02:00:00 PST' in row[0] and 'house_99_ELEC' in row[1]:
-						# print(curr_val)
-						# print(row[0],row[1],float(house_dict[row[1]][row[0]]))
+					time_dict[row[0]]={row[1]:curr_val}
+	#Swapping keys between timestamp and house
+	# Gather k2-k1-v2
+	g = ((k2,k1,v2) for k1,v1 in time_dict.items() for k2,v2 in v1.items())
+	# Sort by k2
+	sg = sorted(g)
+	# Group by k2
+	gsg = itertools.groupby(sg, key=operator.itemgetter(0))
+	# Turn it into a dict of dicts
+	house_dict = {k: {ksk[1]: ksk[2] for ksk in group} for (k, group) in gsg}
+	# print(b['house_97_ELEC'])
+	# print(max(b['house_97_ELEC'].iteritems(), key=operator.itemgetter(1))[0])
 	for house in house_dict : 
-		# print(house_dict)
-		total_energy = max(float(d) for d in house_dict[house].values())
-		max_curr = (max(float(d) for d in house_dict[house].values())) 
-		curr_list.append(max_curr)
-		house_curr[house]=max_curr
-	print()
+		curr_list_for_print.append([house,max(house_dict[house].items(), key=operator.itemgetter(1))[0],max(house_dict[house].items(), key=operator.itemgetter(1))[1]])
+		curr_list.append(max(house_dict[house].items(), key=operator.itemgetter(1))[1])
+	print(curr_list_for_print)
 	return curr_list 
 
 
@@ -78,7 +85,7 @@ plt.ylabel('Number of Houses')
 plt.xlabel('Electrical Panel Size (A)')
 plt.legend()
 plt.tight_layout()
-# plt.show()
+plt.show()
 
 
 	# f_list.append(file_name)
