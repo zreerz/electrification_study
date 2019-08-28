7be9e81707868d92443cf9d3c4b32dff0f208654
aivanova5 <ivanova.alyona5@gmail.com>
2019-08-28 13:40:50 -0700

    Fixing missing folders for output

--- main.sh
+++ main.sh
@@ -8,6 +8,26 @@ if [ ! -d "output/" ]
 then
     mkdir output
 fi
+if [ ! -d "output/feeder_plot" ] 
+then
+    mkdir output/feeder_plot
+fi
+if [ ! -d "output/feeder_power" ] 
+then
+    mkdir output/feeder_power
+fi
+if [ ! -d "output/line_losses" ] 
+then
+    mkdir output/line_losses
+fi
+if [ ! -d "output/main_node_power" ] 
+then
+    mkdir output/main_node_power
+fi
+if [ ! -d "output/output_loadshapes" ] 
+then
+    mkdir output/output_loadshapes
+fi
 if [ ! -d "elec_config/" ] 
 then
     mkdir elec_config
